074aea9b9861d84481e893d91dd825fbba84b1b3
Joachim Metz <joachim.metz@gmail.com>
2017-07-06 05:56:24 +0200

    Changed validator to check for ** #216

--- tools/validator.py
+++ tools/validator.py
@@ -26,6 +26,26 @@ class ArtifactDefinitionsValidator(object):
     self._artifact_registry = registry.ArtifactDefinitionsRegistry()
     self._artifact_registry_key_paths = set()
 
+  def _CheckPath(self, filename, artifact_definition, path):
+    """Checks a path.
+
+    Args:
+      filename (str): name of the artifacts definition file.
+      artifact_definition (ArtifactDefinition): artifact definition.
+      path (str): path.
+
+    Returns:
+      bool: True if the Registry key path is valid.
+    """
+    result = True
+
+    if u'**' in path:
+      logging.warning(
+          u'Artifact definition: {0} in file: {1} contains path with **'.format(
+              artifact_definition.name, filename))
+
+    return result
+
   def _CheckRegistryKeyPath(self, filename, artifact_definition, key_path):
     """Checks a Windows Registry key path.
 
@@ -40,12 +60,18 @@ class ArtifactDefinitionsValidator(object):
     result = True
     key_path = key_path.upper()
 
+    if u'**' in key_path:
+      logging.warning((
+          u'Artifact definition: {0} in file: {1} contains Windows '
+          u'Registry key path with **').format(
+              artifact_definition.name, filename))
+
     if key_path.startswith(u'%%CURRENT_CONTROL_SET%%'):
-      result = False
       logging.warning((
           u'Artifact definition: {0} in file: {1} contains Windows '
           u'Registry key path that starts with '
-          u'%%CURRENT_CONTROL_SET%%. Replace %%CURRENT_CONTROL_SET%% with '
+          u'%%CURRENT_CONTROL_SET%%. '
+          u'Replace %%CURRENT_CONTROL_SET%% with '
           u'HKEY_LOCAL_MACHINE\\System\\CurrentControlSet').format(
               artifact_definition.name, filename))
 
@@ -104,7 +130,13 @@ class ArtifactDefinitionsValidator(object):
           result = False
 
         for source in artifact_definition.sources:
-          if source.type_indicator == (
+          if source.type_indicator == definitions.TYPE_INDICATOR_FILE:
+
+            for path in source.paths:
+              if self._CheckPath(filename, artifact_definition, path):
+                result = False
+
+          elif source.type_indicator == (
               definitions.TYPE_INDICATOR_WINDOWS_REGISTRY_KEY):
 
             # Exempt the legacy file from duplicate checking because it has
